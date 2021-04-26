d12b3c0d0e9ffa4f3bcfb262b9b54880998f1946
Avraz  Zebary <avrazebary@gmail.com>
2021-04-26 12:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
-    .then(resp => resp.json())
-    .then(json => renderBooks(json))
+  // To pass the tests, don't forget to return your fetch!
+ return  fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => console.log(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
